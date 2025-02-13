1e800f3eb477dd667ae0a4638780125186963107
michaelclemens <michaelclemens8@hotmail.com>
2025-02-13 15:03:44 +0000

    Adding tooling for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/space-tourism-multipage-website/',
   plugins: [react(), tailwindcss(), checker({ typescript: true, eslint: { lintCommand: 'eslint .', useFlatConfig: true } })],
   css: {
     modules: {
